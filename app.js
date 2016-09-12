5da29bb5f0c7a1de6855a1b2c2b472c5711b4186
Simon Budin <simon.budin@transparency-one.com>
2016-09-12 07:28:30 +0200

    Refactor to functions

--- app.js
+++ app.js
@@ -26,221 +26,239 @@ var formatNumber = d3.format(",.0f"), // zero decimal places
   },
   color = d3.scale.category20();
 
-// append the svg canvas to the page
-var svg = d3.select("#vis").append("svg")
-  .attr("width", width + MARGINS.LEFT + MARGINS.RIGHT)
-  .attr("height", height + MARGINS.TOP + MARGINS.BOTTOM)
-  .append("g")
-  .attr("transform",
-    "translate(" + MARGINS.LEFT + "," + MARGINS.TOP + ")");
+var svg = drawSvg();
 var sankey = {};
-var path = {};
+var pathTemplate = {};
+var graphData = getData(0);
+resetSankey();
+draw();
 
-// Set the sankey diagram properties
+function drawSvg() {
+  // append the svg canvas to the page
+  var svg = d3.select("#vis").append("svg")
+    .attr("width", width + MARGINS.LEFT + MARGINS.RIGHT)
+    .attr("height", height + MARGINS.TOP + MARGINS.BOTTOM)
+    .append("g")
+    .attr("transform",
+      "translate(" + MARGINS.LEFT + "," + MARGINS.TOP + ")");
+  return svg;
+}
+
 function resetSankey() {
+  // remove all svg, start clean
   svg.selectAll("svg *").remove();
+  // Set the sankey diagram properties
   sankey = d3sankey()
     .nodeWidth(NODE_WIDTH)
     .nodePadding(NODE_PADDING)
     .size([width, height]);
-
-
 }
 
-function relayout() {
-  console.log('relayout');
-  sankey.relayout();
-}
-
-// load the data
-var graph = getData(0);
-resetSankey();
-draw();
-
-function addNode() {
+function addSankeyNode() {
   console.log('adding node');
   var nodeDef = {
-    "node": graph.nodes.length,
-    "name": "node" + (graph.nodes.length + 1)
+    "node": graphData.nodes.length,
+    "name": "node" + (graphData.nodes.length + 1)
   };
   var linkDef = {
     "source": nodeDef.node - 1,
     "target": nodeDef.node,
     "value": 10
   };
-  graph.nodes.push(nodeDef);
-  graph.links.push(linkDef);
+  graphData.nodes.push(nodeDef);
+  graphData.links.push(linkDef);
   resetSankey();
   draw();
 }
 
 function draw() {
 
-  sankey.nodes(graph.nodes)
-    .links(graph.links)
+  sankey.nodes(graphData.nodes)
+    .links(graphData.links)
     .layout(1);
 
+  drawConnections();
+  drawNodes();
 
-  //  ╦  ┬┌┐┌┬┌─┌─┐
-  //  ║  ││││├┴┐└─┐
-  //  ╩═╝┴┘└┘┴ ┴└─┘
-  path = d3.svg.diagonal()
-    .source(function(d) {
-      var xOffset = d.source.type === '_expander' ? EXPANDER_RADIUS + NODE_WIDTH / 2 : NODE_WIDTH;
-      return {
-        "x": d.source.y + d.source.dy / 2,
-        "y": d.source.x + xOffset - 3 
-      };
-    })
-    .target(function(d) {
-      var xOffset = d.target.type === '_expander' ? NODE_WIDTH / 2 - EXPANDER_RADIUS  : 0;
-      return { 
-        "x": d.target.y + d.target.dy / 2,
-        "y": d.target.x + xOffset + 3 /* + sankey.nodeWidth() / 2*/
-      };
-    })
-    .projection(function(d) {
-      return [d.y, d.x];
-    });
-  var link = svg.append("g").selectAll(".link")
-    .data(graph.links)
-    .enter().append("path")
-    .attr("class", "link")
-    .attr("d", path)
-    .style("stroke-width", function(d) {
-      return Math.max(1, Math.sqrt(d.dy));
-    })
-    // .sort(function(a, b) {
-    //   return b.dy - a.dy;
-    // })
-  ;
-  link.append("title")
-    .text(function(d) {
-      return d.source.name + " → " + d.target.name + "\n" + format(d.value);
-    });
+  function drawConnections() {
+    pathTemplate = buildPathTemplates();
+    drawLinks();
 
+    function buildPathTemplates() {
+      var pathTemplate = d3.svg.diagonal()
+        .source(function(d) {
+          var xOffset = d.source.type === '_expander' ? EXPANDER_RADIUS + NODE_WIDTH / 2 : NODE_WIDTH;
+          return {
+            "x": d.source.y + d.source.dy / 2,
+            "y": d.source.x + xOffset - 3
+          };
+        })
+        .target(function(d) {
+          var xOffset = d.target.type === '_expander' ? NODE_WIDTH / 2 - EXPANDER_RADIUS : 0;
+          return {
+            "x": d.target.y + d.target.dy / 2,
+            "y": d.target.x + xOffset + 3
+          };
+        })
+        .projection(function(d) {
+          return [d.y, d.x];
+        });
+        return pathTemplate;
+    }
 
-  //  ╔╗╔┌─┐┌┬┐┌─┐┌─┐ 
-  //  ║║║│ │ ││├┤ └─┐
-  //  ╝╚╝└─┘─┴┘└─┘└─┘
-  var node = svg.append("g").selectAll(".node")
-    .data(graph.nodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) {
-      console.log(d.name, d.x, d.y);
-      return "translate(" + d.x + "," + d.y + ")";
-    })
-    .call(d3.behavior.drag()
-      .origin(function(d) {
-        return d;
-      })
-      .on("dragstart", function() {
-        this.parentNode.appendChild(this);
-      })
-      .on("drag", dragmove));
-
-
-  if (SHOW_DEBUG_BOX) {
-    node
-      .append("rect")
-      .style("fill", "none")
-      .style("stroke-width", 1)
-      .style("stroke", "#F00")
-      .attr("width", sankey.nodeWidth())
-      .attr("height", function(d) {
-        return d.dy;
-      });
+    function drawLinks() {
+      link = svg.append("g").selectAll(".link")
+        .data(graphData.links)
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", pathTemplate)
+        .style("stroke-width", function(d) {
+          return Math.max(1, Math.sqrt(d.dy));
+        });
+      link.append("title")
+        .text(function(d) {
+          return d.source.name + " → " + d.target.name + "\n" + format(d.value);
+        });
+    }
   }
 
-  //  ╔═╗┬─┐┌─┐┌┬┐┬ ┬┌─┐┌┬┐
-  //  ╠═╝├┬┘│ │ │││ ││   │ 
-  //  ╩  ┴└─└─┘─┴┘└─┘└─┘ ┴ 
+  function drawNodes() {
+    node = putNodesInPlace();
+    drawDebugBoxes();
+    drawProductBoxes();
+    drawProductIcons();
+    drawExpanderBoxes();
+    drawProductTexts();
 
-  node
-    .filter(function(d) {
-      return (d.type !== "_expander");
-    })
-    .append("rect")
-    .attr("y", function(d) {
-      return d.dy / 2 - BOX_HEIGHT / 2;
-    })
-    .attr("height", BOX_HEIGHT)
-    .attr("rx", BOX_BORDER_RADIUS)
-    .attr("ry", BOX_BORDER_RADIUS)
-    .attr("width", sankey.nodeWidth())
-    //.style("stroke-width", 3)
-    .append("title")
-    .text(function(d) {
-      return d.name + "\n" + format(d.value);
-    });
+    function putNodesInPlace() {
+      var node = svg.append("g").selectAll(".node")
+        .data(graphData.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) {
+          console.log(d.name, d.x, d.y);
+          return "translate(" + d.x + "," + d.y + ")";
+        })
+        .call(d3.behavior.drag()
+          .origin(function(d) {
+            return d;
+          })
+          .on("dragstart", function() {
+            this.parentNode.appendChild(this);
+          })
+          .on("drag", dragmove));
+          return node;
+    }
 
-  node
-    .filter(function(d) {
-      return (d.type !== "_expander");
-    })
-    .append("svg:image")
-    .attr("xlink:href",  'https://cdn2.iconfinder.com/data/icons/freecns-cumulus/32/519791-101_Warning-128.png')
-    .attr("transform", function(d) {
-      return "translate(" + (NODE_WIDTH - 32 - 3) + "," + (d.dy / 2 - BOX_HEIGHT / 2 + 3) +")";
-    })
-    .attr("height", 32)
-    .attr("width",32);
+    function drawDebugBoxes() {
+      if (SHOW_DEBUG_BOX) {
+        node
+          .append("rect")
+          .style("fill", "none")
+          .style("stroke-width", 1)
+          .style("stroke", "#F00")
+          .attr("width", sankey.nodeWidth())
+          .attr("height", function(d) {
+            return d.dy;
+          });
+      }
+    }
 
+    function drawProductBoxes() {
+      node
+        .filter(function(d) {
+          return (d.type !== "_expander");
+        })
+        .append("rect")
+        .attr("y", function(d) {
+          return d.dy / 2 - BOX_HEIGHT / 2;
+        })
+        .attr("height", BOX_HEIGHT)
+        .attr("rx", BOX_BORDER_RADIUS)
+        .attr("ry", BOX_BORDER_RADIUS)
+        .attr("width", sankey.nodeWidth())
+        //.style("stroke-width", 3)
+        .append("title")
+        .text(function(d) {
+          return d.name + "\n" + format(d.value);
+        });
+    }
 
-  //  ╔═╗─┐ ┬┌─┐┌─┐┌┐┌┌┬┐┌─┐┬─┐ 
-  //  ║╣ ┌┴┬┘├─┘├─┤│││ ││├┤ ├┬┘
-  //  ╚═╝┴ └─┴  ┴ ┴┘└┘─┴┘└─┘┴└─
-  node
-    .filter(function(d) {
-      return (d.type === "_expander");
-    })
-    .append("circle")
-    .attr("cx", sankey.nodeWidth() / 2)
-    .attr("cy", function(d) {
-      return d.dy / 2;
-    })
-    .attr("r", EXPANDER_RADIUS)
-    .append("title")
-    .text(function(d) {
-      return d.name + "\n" + format(d.value);
-    });
+    function drawProductIcons() {
+      node
+        .filter(function(d) {
+          return (d.type !== "_expander");
+        })
+        .append("svg:image")
+        .attr("xlink:href", 'https://cdn2.iconfinder.com/data/icons/freecns-cumulus/32/519791-101_Warning-128.png')
+        .attr("transform", function(d) {
+          return "translate(" + (NODE_WIDTH - 32 - 3) + "," + (d.dy / 2 - BOX_HEIGHT / 2 + 3) + ")";
+        })
+        .attr("height", 32)
+        .attr("width", 32);
+    }
 
+    function drawExpanderBoxes() {
+      //  ╔═╗─┐ ┬┌─┐┌─┐┌┐┌┌┬┐┌─┐┬─┐ 
+      //  ║╣ ┌┴┬┘├─┘├─┤│││ ││├┤ ├┬┘
+      //  ╚═╝┴ └─┴  ┴ ┴┘└┘─┴┘└─┘┴└─
+      node
+        .filter(function(d) {
+          return (d.type === "_expander");
+        })
+        .append("circle")
+        .attr("cx", sankey.nodeWidth() / 2)
+        .attr("cy", function(d) {
+          return d.dy / 2;
+        })
+        .attr("r", EXPANDER_RADIUS)
+        .append("title")
+        .text(function(d) {
+          return d.name + "\n" + format(d.value);
+        });
+    }
 
-  //  ╔╦╗┌─┐─┐ ┬┌┬┐
-  //   ║ ├┤ ┌┴┬┘ │ 
-  //   ╩ └─┘┴ └─ ┴ 
-  node
-    .filter(function(d) {
-      return d.type !== '_expander';
-    })
-    .append("text")
-    .attr('class', 'title')
-    .attr("y", function(d) {
-      return d.dy / 2;
-    })
-    .attr("x", BOX_TEXT_MARGIN_LEFT)
-    .text(function(d) {
-      return d.name;
-    })
-    .attr("text-anchor", "start");
-  node
-    .filter(function(d) {
-      return d.type !== '_expander';
-    })
-    .append("text")
-    .attr('class', 'owner')
-    .attr("y", function(d) {
-      return d.dy / 2;
-    })
-    .attr("dy", "1em")
-    .attr("dx", ".2em")
-    .attr("x", BOX_SUBTEXT_MARGIN_LEFT)
-    .text(function(d) {
-      return d.owner;
-    })
-    .attr("text-anchor", "start");
+    function drawProductTexts() {
+      drawProductName();
+      drawProductCompany();
 
+      function drawProductName() {
+        node
+          .filter(function(d) {
+            return d.type !== '_expander';
+          })
+          .append("text")
+          .attr('class', 'title')
+          .attr("y", function(d) {
+            return d.dy / 2;
+          })
+          .attr("x", BOX_TEXT_MARGIN_LEFT)
+          .text(function(d) {
+            return d.name;
+          })
+          .attr("text-anchor", "start");
+      }
+
+      function drawProductCompany() {
+        node
+          .filter(function(d) {
+            return d.type !== '_expander';
+          })
+          .append("text")
+          .attr('class', 'owner')
+          .attr("y", function(d) {
+            return d.dy / 2;
+          })
+          .attr("dy", "1em")
+          .attr("dx", ".2em")
+          .attr("x", BOX_SUBTEXT_MARGIN_LEFT)
+          .text(function(d) {
+            return d.owner;
+          })
+          .attr("text-anchor", "start");
+      }
+    }
+  }
 
   // the function for moving the nodes
   function dragmove(d) {
@@ -249,6 +267,6 @@ function draw() {
         d.x = Math.max(0, Math.min(width - d.dx, d3.event.x))) + "," + (
         d.y = Math.min(height - Math.sqrt(d.dy), d3.event.y)) + ")");
     sankey.relayout();
-    link.attr("d", path);
+    link.attr("d", pathTemplate);
   };
 }
